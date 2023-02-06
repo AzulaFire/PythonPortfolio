4abb7cbbf9e6ce2f41da2785a2549d9497d10dca
John Horn <jhornjr@gmail.com>
2023-02-06 16:13:27 +0900

    SimpleGui Task Manager

--- ToDo-CLI/main.py
+++ ToDo-CLI/main.py
@@ -1,42 +1,13 @@
-from functions import *
+import functions
+import PySimpleGUI as sg
 
-#CLI TO-DO List
 
-while True:
-  user_action = input("\nType add, edit, complete, show or exit: ").lower().strip()
+# Create the window
+window = sg.Window("TO-DO Task Manager", layout="")
 
-  try:
 
-    if user_action == 'add':
-      add()
-    elif user_action == 'show':
-      show()
-    elif user_action == 'edit':
-      todos = show()
 
-      number = getNumber('edit')
-      new_value = input("\nEnter the new value: ")
-      todos[int(number) - 1] = new_value.title() + "\n"
 
-      with open(TODOS_PATH, 'w') as file:
-        file.writelines(todos)
 
-      print("\nThe to-do has been updated.\n")
-    elif user_action == 'complete':
-      todos = show()
 
-      number = getNumber('complete')
-      todo_to_remove = todos[int(number) -1].strip('\n')
-      with open(TODOS_PATH, 'w') as file:
-        todos.pop(int(number) - 1)
-        file.writelines(todos)
 
-      print(f"\nTodo '{todo_to_remove}' was completed and removed from the list.\n")
-    elif user_action == 'exit':
-      print("\nBye")
-      break
-    else:
-      print("Incorrect response provided.")
-
-  except ValueError:
-    pass
