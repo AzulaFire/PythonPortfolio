4e808a2c627d957c08c3c59201d477c8f5659a51
John Horn <jhornjr@gmail.com>
2023-02-10 10:38:23 +0900

    Added columns and configured layout

--- Portfolio/main.py
+++ Portfolio/main.py
@@ -1,12 +1,27 @@
 import json
 import streamlit as st
 
+# HOME PAGE --------------
 
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+  st.image("assets/images/photo.png")
+
+with col2:
+  st.title("John Horn Jr.")
+  content = """Hi, I am John. I am a Python programmer, designer, photographer and artist. I graduated from the University of Phoenix with a degree in Marketing. I have worked as a software engineer in the US and Japan. Though Python is my first love, I can code in many other languages as well. I speak native english and conversational Japanese."""
+  st.info(content)  
+
+# Get Data
 with open('data.json') as file:
   content = file.read()
 
 apps = json.loads(content)
 
+# Loop through data
 for app in apps:
   st.text(app['title'])
   st.text(app['description'])
